c45c9acf7072c27026e71ab1a7408072286cffcf
Prabudh Mishra <prabudh.mishra18@gmail.com>
2021-09-23 23:20:24 +0530

    Project creation and firebase setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dashboard'>
+      <h1 style={{ color: '#6EC177', margin: 0 }}>Hello React dashboard</h1>
+      <p>Application coming soon</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const h1Element = screen.getByText(/Hello React dashboard/i)
+  expect(h1Element).toBeInTheDocument()
+})
